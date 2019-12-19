6d7db5cd7e29c7203445c1b806d407f83070b8ad
tan-nate <nktan93@gmail.com>
2019-12-19 14:01:07 -0800

    removed show commenst controller action

--- happygarden-backend/app/controllers/comments_controller.rb
+++ happygarden-backend/app/controllers/comments_controller.rb
@@ -2,9 +2,4 @@ class CommentsController < ApplicationController
     def create
         comment = Comment.create(content: params[:content], plant_id: params[:plant_id])
     end
-
-    def show
-        comment = Comment.find(params[:id])
-        render json: CommentSerializer.new(comment).to_serialized_json
-    end
 end
